96b7aca206409aeabb7acefd26c46cfbadcc9d35
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-02-08 05:17:40 +0700

    create new API endpoint for `getSingleMovie`

--- src/main/java/id/masadamsahid/movies/MovieService.java
+++ src/main/java/id/masadamsahid/movies/MovieService.java
@@ -4,14 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
+
     public List<Movie> allMovies(){
         List<Movie> movies = movieRepository.findAll();
         System.out.println(movies);
         return movies;
+    }
+
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
