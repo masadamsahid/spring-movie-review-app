d6159c27d63558d95912cd2be71f2ff9d5654df8
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-02-12 04:57:01 +0700

    setting up `CORS`

--- src/main/java/id/masadamsahid/movies/MoviesApplication.java
+++ src/main/java/id/masadamsahid/movies/MoviesApplication.java
@@ -2,12 +2,26 @@ package id.masadamsahid.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
+	}
+	
+	//	add this method
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 
